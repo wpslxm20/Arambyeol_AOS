f1cf19cf36803488d1acfa31c21c6f548b7b8ac6
sumin <gosumin0710@naver.com>
2023-12-04 23:09:26 +0900

    refactor : 텍스트 strings 파일로 옮김

--- app/src/main/java/com/my_app/arambyeol/view/BottomView.kt
+++ app/src/main/java/com/my_app/arambyeol/view/BottomView.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
@@ -18,12 +19,7 @@ import androidx.compose.ui.unit.sp
 import com.my_app.arambyeol.R
 
 object BottomView {
-    private const val FIRST_EXPLANATION =
-        "매주 월요일 1시에 식단표가 업데이트 됩니다!"
-    private const val MIDDLE_EXPLANATION = "앱 아람별 문의사항은 "
-    private const val FINAL_EXPLANATION = " 로 보내주세요 :)"
-    private const val EMAILADDR = "gogosusumlnmln@gmail.com"
-    private const val FONT_SIZE = 11
+    private val FONT_SIZE = 11
 
     @Composable
     fun main() {
@@ -36,27 +32,33 @@ object BottomView {
 
     @Composable
     private fun explanationText() {
+        val firstExplanation =
+            stringResource(id = R.string.bottom_first_explanation)
+        val middleExplanation = stringResource(id = R.string.bottom_middle_explanation)
+        val finalExplanation = stringResource(id = R.string.bottom_final_explanation)
+
+
         Column(
             modifier = Modifier
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "$FIRST_EXPLANATION",
+                text = "$firstExplanation",
                 fontSize = FONT_SIZE.sp,
                 color = colorResource(id = R.color.dark_gray),
                 textAlign = TextAlign.Center
             )
             Row {
                 Text(
-                    text = "$MIDDLE_EXPLANATION",
+                    text = "$middleExplanation",
                     fontSize = FONT_SIZE.sp,
                     color = colorResource(id = R.color.dark_gray),
                     textAlign = TextAlign.Center
                 )
                 SendEmailButton()
                 Text(
-                    text = "$FINAL_EXPLANATION",
+                    text = "$finalExplanation",
                     fontSize = FONT_SIZE.sp,
                     color = colorResource(id = R.color.dark_gray),
                     textAlign = TextAlign.Center
@@ -68,13 +70,15 @@ object BottomView {
 
     @Composable
     private fun SendEmailButton() {
+        val emailAddr = stringResource(id = R.string.bottom_email_addr)
+
         val context = LocalContext.current
         Text(
-            text = EMAILADDR,
+            text = emailAddr,
             modifier = Modifier.clickable(
                 onClick = {
                     val intent = Intent(Intent.ACTION_SENDTO).apply {
-                        data = Uri.parse("mailto:$EMAILADDR")
+                        data = Uri.parse("mailto:$emailAddr")
                     }
 
                     if (intent.resolveActivity(context.packageManager) != null) {
